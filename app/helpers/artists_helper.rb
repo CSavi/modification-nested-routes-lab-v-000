0d87bb54f1eb3ffd69867f02284c9e5890488dfe
Carrie Savinell <carrielane89@gmail.com>
2018-08-08 16:04:01 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -8,6 +8,7 @@ module ArtistsHelper
     if artist.nil?
       select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
     else
-      
+      artist.name
+    end   
   end
 end
