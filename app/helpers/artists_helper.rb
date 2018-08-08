54fe5a0e14ef96b2d1ca75f82f0124e4e79dedcd
Carrie Savinell <carrielane89@gmail.com>
2018-08-08 14:58:29 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -3,6 +3,7 @@ module ArtistsHelper
     song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
   end
 
+#helper to display drop-down list of artists IF via /songs/id/edit & ONLY display artist name
   def artist_select
-  end   
+  end
 end
