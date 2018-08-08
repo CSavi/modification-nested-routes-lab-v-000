37900c3630f706f1167438948ff8b4fe095a6c2b
Carrie Savinell <carrielane89@gmail.com>
2018-08-08 14:44:53 +0000

    automatically backed up by learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,9 +1,11 @@
 module SongsHelper
 
   def artist_id_field(song)
-    if @song.artist.nil?
-      f.select :artist_id, options_from_collection_for_select(Artist.all, :id, :name)
-    end
-    hidden_field :artist_id
+    if song.artist.nil?
+      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
+    else
+      hidden_field_tag "song[artist_id]", song.artist_id
+    end   
   end   
+  
 end
