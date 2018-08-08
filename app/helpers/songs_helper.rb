a0a82d4b1a845f7fed8f0b61c7cefdbf89549cb8
Carrie Savinell <carrielane89@gmail.com>
2018-08-08 14:42:40 +0000

    automatically backed up by learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,9 @@
 module SongsHelper
 
+  def artist_id_field(song)
+    if @song.artist.nil?
+      f.select :artist_id, options_from_collection_for_select(Artist.all, :id, :name)
+    end
+    hidden_field :artist_id
+  end   
 end
